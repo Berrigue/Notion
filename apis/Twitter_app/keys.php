e7933edc6c890b6437651942d5f7f61832dcfb0f
Alexandre Berrigue <berrigue@gmail.com>
2015-11-24 00:16:26 -0200

    Update keys.php

--- apis/Twitter_app/keys.php
+++ apis/Twitter_app/keys.php
@@ -2,10 +2,10 @@
 
 //metodos que recebem as chaves de acesso do Twiiter
 
-$consumer = "CZi77fp1mNpVFgfeehOjtpa9H";
-$consumersecret = "H20CWrhxZuU4BkKuzceEZ0vCWloonsb8bWu8EJrokauJ58RhLQ";
-$accesstoken = "313623681-xpd0UxtCyZB2YRcFQ6vsCw3LWq8xIrwGH2bHjtvB";
-$accesstokensecret = "4jJkwHXcodz5251e9hQedOFTC7kqNzOPAyXwY2wgV87TV";
+$consumer = "";
+$consumersecret = "";
+$accesstoken = "";
+$accesstokensecret = "";
 
 $twitter = new TwitterOAuth($consumer,$consumersecret,$accesstoken,$accesstokensecret);
 
